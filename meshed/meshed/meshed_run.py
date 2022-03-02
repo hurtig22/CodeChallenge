2a7d4f1e050f110ce1022afb7c0f43a3bd74b3a8
hurtig22 <100345697+hurtig22@users.noreply.github.com>
2022-03-02 15:54:09 +0100

    Little faster script

--- meshed/meshed/meshed_run.py
+++ meshed/meshed/meshed_run.py
@@ -21,12 +21,19 @@ def run_json_mesh_file(mesh, N):
         """
         return abs(a-b) <= max(rel_tol * max(abs(a), abs(b)), abs_tol)
 
+    def combine_lists_and_remove_duplicates(list1, list2):
+        newlist = list1 + list(set(list2) - set(list1))
+        return newlist
+
     elements = mesh.get_elements()
     nodes = mesh.get_nodes()
     values = mesh.get_values()
 
     # {element_id: element_id1, value: <number value>},
     spots = []
+
+    # list of elementids not to be seen again
+    donotlookAt = []
 
     for element in elements:
 
@@ -43,21 +50,36 @@ def run_json_mesh_file(mesh, N):
             list_new = [ ele['id'] for ele in elements if ( en in ele['nodes'])]
             list_all_new=list(set().union(list_all_new,list_new))
 
+        # element_id = id is also found, so remove that one
         list_all_new.remove(id)
 
-        #print(id, list_all_new)
-
         # get values for element IDs in second list
-        values_new = [ val['value'] for  val in values if (val['element_id'] in list_all_new)]
-        #print(values_new)
+        values_new = [ {'element_id': val['element_id'], 'value' : val['value']}
+                  for  val in values if (val['element_id'] in list_all_new)]
 
-        # compare value in element to all neigbor values
+        # get value for element_id = id
         value_node =[ val['value'] for  val in values if (val['element_id'] == id)][0]
 
-        if (value_node > max(values_new)):
-            #print(id, ' ', value_node)
+        # compare value in element (value_node) to max of all neigbor values (values_new)
+        max_value_new = max([d['value'] for d in values_new])
+
+        if (value_node > max_value_new):
+            # setup output view spot dict
             spot = {'element_id': id, 'value': value_node}
             spots.append(spot)
+
+            # all element ids cannot be maxima, so append to donotlookAt
+            donotlookAt = combine_lists_and_remove_duplicates(donotlookAt, list_all_new)
+
+        # put all elements smaller than max to donotlookAt
+        else:
+            # get index of max value
+            max_value_new_id = [ val['element_id'] for  val in values if (val['value'] == max_value_new)][0]
+            # remove index of max value from list_all_new
+            #print(max_value_new_id, list_all_new)
+            list_all_new.remove(max_value_new_id)
+            # append all elements smaller than max to donotlookAt
+            donotlookAt = combine_lists_and_remove_duplicates(donotlookAt, list_all_new)
 
             if (len(spots) == N):
                 break
